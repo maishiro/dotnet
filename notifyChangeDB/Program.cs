bc74761449a5ad142d5213a10cbba815f400eb11
maishiro <16446436+maishiro@users.noreply.github.com>
2022-12-01 21:16:40 +0900

    first
    DB設定による切り替え
    
    .NET 6

---
+++ notifyChangeDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+
+var appSettings = ConfigurationManager.AppSettings;
+var app = ConfigurationManager.AppSettings["test"];
+var dbType = ConfigurationManager.AppSettings["DBType"];
+var conString = ConfigurationManager.AppSettings["ConnectionString"];
+
+
+DbAccess db = DbAccess.CreateDB(dbType, conString);
+db.Open();
+db.Run();
