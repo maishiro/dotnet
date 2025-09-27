e6bd29193f0b085c2b72d7f2e833908d641c191c
maishiro <16446436+maishiro@users.noreply.github.com>
2025-09-27 14:07:49 +0900

    AspireApp1 - Aspire Update

--- AspireApp1/AspireApp1.AppHost/Program.cs
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -1,26 +1,33 @@
-using Aspire.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 
-var broker = builder.AddRabbitMQ("broker")
-                    .WithManagementPlugin()
-                    .WithImage("rabbitmq", "3-management")
-                    .WithEnvironment("RABBITMQ_DEFAULT_USER", "admin")
-                    .WithEnvironment("RABBITMQ_DEFAULT_PASS", "admin")
-                    .WithHttpEndpoint(port: 15672, targetPort: 15672);
 
-var lakedb = builder.AddPostgres("lakedb")
+var username = builder.AddParameter("username", secret: true);
+var password = builder.AddParameter("password", secret: true);
+var broker = builder.AddRabbitMQ("broker", username, password)
+                    .WithManagementPlugin();
+
+var lakeUsername = builder.AddParameter("lakeuser", secret: true);
+var lakePassword = builder.AddParameter("lakepass", secret: true);
+var lakedb = builder.AddPostgres("lake", lakeUsername, lakePassword)
                     .WithImage("timescale/timescaledb", "latest-pg15")
-                    .WithEnvironment("POSTGRES_DB", "lakedb")
-                    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
-                    .WithEndpoint( port: 5437, targetPort: 5432, scheme: "tcp", name:"lakedb" );
+                    .AddDatabase("lakedb");
+//var lake = builder.AddContainer("lakedb", "timescale/timescaledb", "latest-pg15")
+//                    .WithEnvironment("POSTGRES_DB", "lakedb")
+//                    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+//                    .WithEndpoint( port: 5437, targetPort: 5432, name:"lakedb" );
+//var lakedb = lake.GetEndpoint("lakedb");
 
-
-var martdb = builder.AddContainer("martdb", "timescale/timescaledb", "latest-pg15")
-                    .WithEnvironment("POSTGRES_DB", "martdb")
-                    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
-                    .WithEndpoint( port: 5438, targetPort: 5432, name:"martdb" );
+var martUsername = builder.AddParameter("martuser", secret: true);
+var martPassword = builder.AddParameter("martpass", secret: true);
+var martdb = builder.AddPostgres("mart", martUsername, martPassword)
+                    .WithImage("timescale/timescaledb", "latest-pg15")
+                    .AddDatabase("martdb");
+//var mart = builder.AddContainer("martdb", "timescale/timescaledb", "latest-pg15")
+//                    .WithEnvironment("POSTGRES_DB", "martdb")
+//                    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+//                    .WithEndpoint( port: 5438, targetPort: 5432, name:"martdb" );
+//var martdb = mart.GetEndpoint("martdb");
 
 var apiservice = builder.AddSpringApp(
 					    "webapi",
@@ -31,6 +38,8 @@ var apiservice = builder.AddSpringApp(
                             Port = 8080,
                             OtelAgentPath = "./",
                         })
+                        .WithReference(lakedb)
+                        .WithReference(martdb)
                         .WithHttpEndpoint( port:8080, targetPort: 8080, name:"webapi", isProxied: false )
                         .PublishAsDockerFile(
                         [
@@ -41,6 +50,7 @@ var apiservice = builder.AddSpringApp(
 
 builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
+    .WithReference(broker)
     .WithReference(apiservice);
 
 
