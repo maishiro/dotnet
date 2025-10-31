70cfc30c5495469ab394e05c63a7fec0b772c791
maishiro <16446436+maishiro@users.noreply.github.com>
2025-11-01 00:38:46 +0900

    Add dfSelfHostWebAPI project - Self-hosted WebAPI sample with .NET Framework

---
+++ dfSelfHostWebAPI/dfSelfHostWebAPI/Services/MyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dfSelfHostWebAPI.Services
+{
+    public interface IMyService
+    {
+        string Greet( string name );
+    }
+
+    public class MyService : IMyService
+    {
+        public string Greet( string name ) => $"Hello {name}, from SelfHost!";
+    }
+}
